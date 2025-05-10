55c6a1ce9ba98be3a992dc25330ecba0cb2d6f9f
Tommy <tomasfwat@gmail.com>
2025-05-10 10:37:12 -0300

    Cambios en lanzador

--- Lanzador/JGame.java
+++ Lanzador/JGame.java
@@ -1,3 +1,32 @@
-public class JGame {
+public abstract  class JGame {
+    private String nombre;
+    private String desarrolladores;
+    private String version; 
+    private String descripcion;
+    private boolean implementado;
+
+    public String setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public String getDesarrolladores(){
+        return this.desarrolladores;
+    }
+
+    public void setDesarrolladores(String desarrolladores){
+        this.desarrolladores = desarrolladores; 
+    }
+
+    public void setVersion(String version){
+        this.version=version;
+    }
+
+    public String 
+
+
 
 }
